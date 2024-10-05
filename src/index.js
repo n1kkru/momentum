7dc278629df7c8e1c7f718edad51c10573b8daf0
Nik Krupin <krupin.ns@mail.ru>
2024-10-05 10:33:09 +0300

    add: dynamic img and button 'clear todo'

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import { fetchWeather } from "./api";
 import "./index.css";
 import { changeWeatherIcon } from "./scripts/date";
-import { closeByClick, closeModal, openModal } from "./scripts/modal";
+import { closeByClick, openModal } from "./scripts/modal";
 import { transferDayOfWeek, transferMonth } from "./scripts/time";
 import { createTodo, deleteElement } from "./scripts/todo";
 
@@ -10,6 +10,7 @@ const mainDate = document.querySelector(".main__date");
 const dateValue = mainDate.querySelector(".main__date-date");
 const timeValue = mainDate.querySelector(".main__date-time");
 
+const buttonClear = document.querySelector(".button-clear");
 const buttonTasks = document.querySelector(".button-tasks");
 const iconWeather = document.querySelector(".icon_weather");
 const temperatureValue = document.querySelector(".nav__element-temperature");
@@ -19,7 +20,6 @@ const todosList = document.querySelector('.todos__list');
 const popupTodos = document.querySelector('.popup_type_todo');
 const formAddTodo = document.forms['new-todo'];
 const newTodoName = formAddTodo.elements['todo-name'];
-
 
 findLocation();
 setDefaultValues();
@@ -55,6 +55,13 @@ formAddTodo.addEventListener('submit', (evt) => {
   newTodoName.value = "";
 });
 
+buttonClear.addEventListener('click', () => {
+    Array.from(todosList.children).forEach((element) => {
+      if (element.children.item(0).checked) deleteElement(element);
+    });
+
+})
+
 function findLocation() {
   if (!navigator.geolocation) {
     alert(
@@ -82,6 +89,7 @@ function updateDate() {
   const month = transferMonth(date.getMonth());
   const dayOfWeek = transferDayOfWeek(date.getDay());
 
+  setBackgroundImage(date.toTimeString().slice(0, 2))
   dateValue.textContent = `${day} ${month}, ${dayOfWeek}`;
   timeValue.textContent = date.toTimeString().slice(0, 8);
   setTimeout(function () {
@@ -101,4 +109,23 @@ function setDefaultValues() {
   localStorage.getItem("temperature")
     ? (temperatureValue.textContent = `${localStorage.getItem("temperature")} °C`)
     : (city.textContent = "25 °C");
+}
+
+function setBackgroundImage(hours) {
+  const page = document.querySelector(".page__content");
+  if ( 0 <= Number(hours) && Number(hours) < 6) {
+    page.classList.add('image_night');
+  }
+
+  else if ( 6 <= Number(hours) && Number(hours) < 12) {
+    page.classList.add('image_morning');
+  }
+
+  else if ( 12 <= Number(hours) && Number(hours) < 18) {
+    page.classList.add('image_day');
+  }
+
+  else if ( 18 <= Number(hours) && Number(hours) <= 23) {
+    page.classList.add('image_evening');
+  }
 }
